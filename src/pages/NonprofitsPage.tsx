6e0f0fc5d2acaa06525f7e4f1c321350ee0e11b1
alfonsoocampo <fonsy.ocampo@gmail.com>
2026-08-15 02:45:27 -0700

    Proposal Form Button changed to one line and polaroids shifted up and fixed responsive design just before md

--- src/pages/NonprofitsPage.tsx
+++ src/pages/NonprofitsPage.tsx
@@ -55,7 +55,7 @@ const NonprofitsPage = () => {
           </div>
 
           <div className="relative h-[340px] w-full max-w-[324px] shrink-0 self-center md:h-[420px] md:max-w-[460px] min-[901px]:ml-auto">
-            <div className="absolute left-0 top-[50%] -translate-y-[52%] md:left-[10px]">
+            <div className="absolute left-0 top-[50%] -translate-y-[20%] md:left-[10px]">
               <PolaroidPhoto
                 imageSrc="/images/parter-with-us/stoney-creek.png"
                 caption="salmon release @stoney creek"
@@ -64,7 +64,7 @@ const NonprofitsPage = () => {
                 imageCropClassName="object-center scale-[1.04]"
               />
             </div>
-            <div className="absolute right-0 top-[50%] z-10 -translate-y-[68%]">
+            <div className="absolute -right-24 max-lg:top-[65%] top-[50%] z-10 -translate-y-[68%] ">
               <PolaroidPhoto
                 imageSrc="/images/parter-with-us/OCB_project_handoff.png"
                 caption="project handoff @OCB"
